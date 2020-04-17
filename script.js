06b4e8d654b17ef353726140904985589b4e933d
CodeJesus24 <maksimenko.yr@gmail.com>
2020-04-17 15:37:12 +0200

    js and css for header

---
+++ script.js
@@ -0,0 +1,14 @@
+let nav = document.querySelector(".main-nav");
+let logos = document.querySelectorAll(".logo");
+
+window.addEventListener("scroll", function() {
+    if(window.pageYOffset >= 33){
+        nav.classList.add("main-nav--sticked");
+        logos[0].classList.add("logo--disable");
+        logos[1].classList.remove("logo--disable");
+    } else {
+        nav.classList.remove("main-nav--sticked");
+        logos[1].classList.add("logo--disable");
+        logos[0].classList.remove("logo--disable");
+    }
+});
